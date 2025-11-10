e6735706ece5c717a9dab754d416dc4273fc8ec5
friezy <daumantas.balakauskas@gmail.com>
2025-11-10 03:13:55 +0200

    fastfetch

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,18 +13,19 @@ config.line_height = 1.2 --120%
 
 -- Colors
 config.colors = {
-	cursor_bg = "white",
-	cursor_border = "white",
+    cursor_bg = "white",
+    cursor_border = "white",
 }
 
 -- Appearance
 --config.window_decorations = "RESIZE"
 config.window_background_image = constants.bg_image
+config.window_background_opacity = 0.85
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
 
 -- Misc
