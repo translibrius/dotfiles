fe8613fb188ccfd22211fa212ff0fd536d47ab4f
friezy <daumantas.balakauskas@gmail.com>
2026-03-17 12:42:08 +0200

    feat(alacritty, powershell): windowed mode, ctrl+c/v keybindings, zoxide init and nav aliases

--- windows/powershell/Microsoft.PowerShell_profile.ps1
+++ windows/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,10 @@
 # Initialize Starship prompt
 Invoke-Expression (&starship init powershell)
+
+# Initialize zoxide (smarter cd)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# Quick navigation aliases
+function dots { Set-Location "C:\Users\friezy\Desktop\works\personal_projects\dotfiles" }
+function proj { Set-Location "C:\Users\friezy\Desktop\works\personal_projects" }
+function realm { Set-Location "C:\Users\friezy\Desktop\works\personal_projects\c\Realm" }
